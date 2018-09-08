0dc7f124f5f5b664b63f7a10e6fdfa687def96cd
MrCharlss <camachotcarlos@gmail.com>
2018-09-08 01:51:36 -0500

    primera parte del bot

---
+++ bot.js
@@ -0,0 +1,9 @@
+console.log("bot is running");
+let Twit = require("twit");
+
+var T = new Twit({
+  consumer_key: "...",
+  consumer_secret: "...",
+  access_token: "...",
+  access_token_secret: "..."
+});
